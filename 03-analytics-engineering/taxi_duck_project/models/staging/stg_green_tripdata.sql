cb9acdcc2bbdffd996cfe370705bdfe097c1a430
Nicholas <nicholasanayaputraa@gmail.com>
2025-04-19 20:19:45 +0700

    fix: structure folder

--- 03-analytics-engineering/taxi_duck_project/models/staging/stg_green_tripdata.sql
+++
@@ -1,7 +0,0 @@
--- models/staging/stg_green_tripdata.sql
-
-with source_data as (
-    select * from read_csv_auto('C:\data-engineer-2025\03-analytics-engineering\taxi_duck_project\dataset\green_tripdata_2019-01.csv')
-)
-
-select * from source_data
